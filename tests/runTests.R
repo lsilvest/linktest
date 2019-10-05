95f2a65edba428330b7d5ff1833f7d5609103e4a
lsilvest <lsilvestri@ztsdb.org>
2019-10-04 23:20:37 -0400

    make test independent of timezone in which it is run; remove checked-in compilation artifacts

--- tests/runTests.R
+++ tests/runTests.R
@@ -1,5 +1,14 @@
 library(linktest)
+
 res <- linktest::convertToSeconds(2019, 9, 9, 12, 12, 12, "America/New_York")
-if (as.POSIXct(res, origin="1970-01-01") != "2019-09-09 12:12:12 EDT") 
+print(res)
+
+resPOSIXct <- as.POSIXct(res, origin="1970-01-01")
+print(resPOSIXct)
+
+resChar <- format(resPOSIXct, tz="America/New_York")
+print(resChar)
+
+if (resChar != "2019-09-09 12:12:12") 
   stop("error in convert function")
 
